42daec766458a19b3acf81386336aa7c4fb3185e
jaganarasu <jaganarasu.aj@gmail.com>
2024-12-16 00:54:59 +0530

    image path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -94,7 +94,7 @@ const Footer = () => {
   <div className="col-md-6">
     <h5>WE ACCEPT</h5>
     <img
-      src="..\src\image\pay.png" // Adjust the path to your image
+      src="../src/image/pay.png" // Adjust the path to your image
       alt="Payment Methods"
       className="img-fluid" // Makes the image responsive
     />
